8c7c3baf58815e75a86365e2017497af3d0a81ee
johannheyne <mail@johannheyne.de>
2014-06-04 17:56:39 +0200

    [CHANGE] package name

--- toolset.php
+++ toolset.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package   Toolset
+ * @package   WordPressToolset
  * @author    Your Name mail@johannheyne
  * @license   GPL-2.0+
  * @link      https://github.com/johannheyne/wordpress-toolset
@@ -10,7 +10,7 @@
  * Plugin Name:       Toolset
  * Plugin URI:        https://github.com/johannheyne/wordpress-toolset
  * Description:       Provides functionality fore use in themes.
- * Version:           0.0.6
+ * Version:           0.0.7
  * Author:            Johann Heyne
  * Author URI:        www.johannheyne.de
  * Text Domain:       toolset
