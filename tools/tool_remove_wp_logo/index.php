1c172ea410a389ec6a444432e34ea8afc07df256
johannheyne <mail@johannheyne.de>
2014-06-12 19:18:18 +0200

    [UPDATE] tool_remove_wp_logo
    |NEW] remove logo from loginpage

--- tools/tool_remove_wp_logo/index.php
+++ tools/tool_remove_wp_logo/index.php
@@ -1,10 +1,31 @@
 <?php
 
-	add_action( 'admin_bar_menu', 'tool_remove_wp_logo', 999 );
+	// ADMINBAR {
 
-	function tool_remove_wp_logo( $wp_admin_bar ) {
-		
-		$wp_admin_bar->remove_node( 'wp-logo' );
-	}
+		function tool_remove_wp_logo_adminbar( $wp_admin_bar ) {
+
+			$wp_admin_bar->remove_node( 'wp-logo' );
+		}
+
+		add_action( 'admin_bar_menu', 'tool_remove_wp_logo_adminbar', 999 );
+
+	// }
+
+	// LOGINPAGE {
+
+		function tool_remove_wp_logo_loginpage() {
+			echo '<style type="text/css">
+				h1 a { 
+					display: none !important;
+				}
+				h1:after {
+					content: "' . get_bloginfo( 'name' ) . '";
+				}
+			</style>';
+		}
+
+		add_action( 'login_head', 'tool_remove_wp_logo_loginpage' );
+
+	// }
 
 ?>
