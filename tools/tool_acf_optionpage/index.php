47346047e89cf1fd3db4e89dce5d242e3f74a931
johannheyne <mail@johannheyne.de>
2014-08-21 13:27:28 +0200

    acf define optionpage without fields

--- tools/tool_acf_optionpage/index.php
+++ tools/tool_acf_optionpage/index.php
@@ -62,72 +62,76 @@
 
 					$lang_array = unserialize( THEME_LANG_ARRAY );
 
-					foreach ( $p['setup']['fieldgroups'] as $key1 => $fg ) {
+					if ( isset( $p['setup']['fieldgroups'] ) && count( $p['setup']['fieldgroups'] ) > 0 ) {
 
-						$fields = array();
+						foreach ( $p['setup']['fieldgroups'] as $key1 => $fg ) {
 
-						foreach ( $lang_array as $key => $item ) {
+							$fields = array();
 
-							// LANGUAGE TAB
+							foreach ( $lang_array as $key => $item ) {
 
-							$fields[] = array (
-								'key' => 'field_opt_' . $p['setup']['optionpage_key'] . '_' . $fg['key'] . '_tab_' . $item['language_code'],
-								'label' => $item['translated_name'],
-								'name' => '',
-								'type' => 'tab',
-							);
+								// LANGUAGE TAB
 
-							// FIELDS {
+								$fields[] = array (
+									'key' => 'field_opt_' . $p['setup']['optionpage_key'] . '_' . $fg['key'] . '_tab_' . $item['language_code'],
+									'label' => $item['translated_name'],
+									'name' => '',
+									'type' => 'tab',
+								);
 
-							foreach ( $fg['fields'] as $key => $item2 ) {
+								// FIELDS {
 
-								if ( $item2['type'] == 'text' ) {
+								foreach ( $fg['fields'] as $key => $item2 ) {
 
-									$fields[] = array (
-										'key' => 'field_opt_' . $p['setup']['optionpage_key'] . '_' . $fg['key'] . '_' . $item2['key'] . '_' . $item['language_code'],
-										'label' => $item2['label'],
-										'name' => 'opt_' . $p['setup']['optionpage_key'] . '_' . $fg['key'] . '_' . $item2['key'] . '_' . $item['language_code'],
-										'type' => 'text',
-										'required' => 1,
-										'default_value' => $item2['default_value'],
-										'formatting' => 'none',
-									);
+									if ( $item2['type'] == 'text' ) {
+
+										$fields[] = array (
+											'key' => 'field_opt_' . $p['setup']['optionpage_key'] . '_' . $fg['key'] . '_' . $item2['key'] . '_' . $item['language_code'],
+											'label' => $item2['label'],
+											'name' => 'opt_' . $p['setup']['optionpage_key'] . '_' . $fg['key'] . '_' . $item2['key'] . '_' . $item['language_code'],
+											'type' => 'text',
+											'required' => 1,
+											'default_value' => $item2['default_value'],
+											'formatting' => 'none',
+										);
+									}
 								}
+
+								// FIELDS }
+
 							}
 
-							// FIELDS }
+							// }
+							//print_o( $fields );
 
+							// INIT FIELDS {
+
+								register_field_group( array (
+									'id' => 'acf_' . $p['setup']['optionpage_key'] . '_' . $fg['key'],
+									'title' => $fg['label'],
+									'fields' => $fields,
+									'location' => array (
+										'rules' => array (
+											array (
+												'param' => 'options_page',
+												'operator' => '==',
+												'value' => 'acf-options-' . strtolower( $p['setup']['optionpage_name'] ),
+												'order_no' => 0,
+											),
+										),
+										'allorany' => 'all',
+									),
+									'options' => array (
+										'position' => 'normal',
+										'layout' => 'default',
+										'hide_on_screen' => array (),
+									),
+									'menu_order' => 999,
+								));
+
+							// }
 						}
 
-						// }
-						//print_o( $fields );
-
-						// INIT FIELDS {
-
-							register_field_group( array (
-								'id' => 'acf_' . $p['setup']['optionpage_key'] . '_' . $fg['key'],
-								'title' => $fg['label'],
-								'fields' => $fields,
-								'location' => array (
-									'rules' => array (
-										array (
-											'param' => 'options_page',
-											'operator' => '==',
-											'value' => 'acf-options-' . strtolower( $p['setup']['optionpage_name'] ),
-											'order_no' => 0,
-										),
-									),
-									'allorany' => 'all',
-								),
-								'options' => array (
-									'position' => 'normal',
-									'layout' => 'default',
-									'hide_on_screen' => array (),
-								),
-								'menu_order' => 999,
-							));
-
-						// }
 					}
 
 				// }
@@ -136,16 +140,16 @@
 		}
 
 	// }
-	
+
 	// ACF BUILD OPTIONPAGES ( Version 1 ) {
-	
+
 	    foreach ( $GLOBALS['toolset']['inits']['tool_acf_optionpage'] as $item ) {
-        
+
 			tool_acf_optionpage_build( array(
 		        'setup' => $item,
 			) );
 	    }
-		
+
 	// }
 
 ?>
