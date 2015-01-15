e63cdd4b5473e86d268f4d3090dee3fb29d5bb57
johannheyne <mail@johannheyne.de>
2015-01-15 13:27:45 +0100

    FIX autoload plugin class YerForm

--- classes.php
+++ classes.php
@@ -6,6 +6,6 @@
 	$GLOBALS['toolset']['autoload_php_classes_plugin']['Tool_Single_CostumPost_Template'] = 'tools/tool_costumpost_templates/class.php';
 	$GLOBALS['toolset']['autoload_php_classes_plugin']['Textile'] = 'tools/tool_textile/textile-2.0.0/classTextile.php';
 	$GLOBALS['toolset']['autoload_php_classes_plugin']['Tool_Dashboard_Widget'] = 'tools/tool_dashboard_widget/class.php';
-	$GLOBALS['toolset']['autoload_php_classes_plugin']['YerForm'] = 'tools/assets/yerform/core/yerform.php';
+	$GLOBALS['toolset']['autoload_php_classes_plugin']['YerForm'] = 'assets/yerform/core/yerform.php';
 	
 ?>
