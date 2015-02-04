2d1a2a38f904b86ceaad12b58ec8f7134ba0486e
johannheyne <mail@johannheyne.de>
2015-02-04 10:20:44 +0100

    FIX Releasenumer

--- toolset.php
+++ toolset.php
@@ -10,7 +10,7 @@
 	Plugin Name:       Toolset
 	Plugin URI:        https://github.com/johannheyne/wordpress-toolset
 	Description:       Provides functionality fore use in themes.
-	Version:           0.0.68.2
+	Version:           0.0.69
 	Author:            Johann Heyne
 	Author URI:        www.johannheyne.de
 	Text Domain:       toolset
