59ebaab3bbd87f5081dd2953158ccd394966aa2e
johannheyne <mail@johannheyne.de>
2015-10-19 12:55:20 +0200

    UPDATE Tool tool_browser_outdated
    - new parameter 'lang' to force a language

--- tools/tool_browser_outdated/index.php
+++ tools/tool_browser_outdated/index.php
@@ -3,7 +3,7 @@
 	// BROWSER OUTDATED {
 
 		// IE 8 Support by Microsoft ends January 12, 2016
-		// http://blogs.msdn.com/b/ie/archive/2014/08/07/stay-up-to-date-with-internet-explorer.aspx
+		// Source: http://blogs.msdn.com/b/ie/archive/2014/08/07/stay-up-to-date-with-internet-explorer.aspx
 
 		function tool_browser_outdated( $p = array() ) {
 
@@ -13,15 +13,23 @@
 					'version' => 7,
 					'message' => array(
 						'en' => 'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.',
-						'de' => 'Dieser Browser ist <strong>veraltet!</strong> <a href="http://browsehappy.com/">Aktualisiere Deinen Browser oder wähle einen anderen Browser</a>.',
+						'de' => 'Dieser Browser ist <strong>veraltet!</strong> <a href="http://browsehappy.com/">Bitte aktualisiere Deinen Browser oder wähle einen anderen Browser.</a>.',
 					),
+					'lang' => false,
 				);
 
 				$p = array_replace_recursive( $defaults, $p );
 
 			// }
 
-			$browseroutdated = $p['message'][ config_get_curr_site_id() ];
+			if ( $p['lang'] ) {
+
+				$browseroutdated = $p['message'][ $p['lang'] ];
+			}
+			else {
+
+				$browseroutdated = $p['message'][ config_get_curr_site_id() ];
+			}
 
 			echo '<!--[if lt IE ' . ( $p['version'] + 1 ) . ']>';
 				echo '<p class="browseroutdated font">' . $browseroutdated . '</p>';
@@ -29,6 +37,5 @@
 		}
 
 	// }
-	
 
 ?>
