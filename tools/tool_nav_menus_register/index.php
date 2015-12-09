4396b2430ae29459a1ae4ce8697987cdf6084851
johannheyne <mail@johannheyne.de>
2015-12-09 09:35:38 +0100

    Update Tool "tool_nav_menus_register"
    - rename parameter "slug" to "location"
    
    Please change this in your configuration!

--- tools/tool_nav_menus_register/index.php
+++ tools/tool_nav_menus_register/index.php
@@ -1,20 +1,25 @@
 <?php
 
-	// MENUS SITE ( Version 2 ) {
+	// MENUS SITE ( Version 3 ) {
 
 		function tool_nav_menus_register() {
 
 			if ( isset( $GLOBALS['toolset']['inits']['tool_nav_menus_register'] ) ) {
-				
+
 				$array = false;
-				
+
 				foreach ( $GLOBALS['toolset']['inits']['tool_nav_menus_register'] as $item ) {
 
-					$array[ $item['slug'] ] = $item['name'];
+					if ( ! isset( $item['location'] ) ) {
+
+						$item['location'] = $item['slug'];
+					}
+
+					$array[ $item['location'] ] = $item['name'];
 				}
-				
+
 				if ( $array ) {
-				    
+
 					register_nav_menus( $array );
 				}
 			}
