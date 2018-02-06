6af29bdbd261f834707b4ae8be3d31b55c208d20
johannheyne <mail@johannheyne.de>
2018-02-06 09:08:03 +0100

    Updates Toll tool_acf_translate
    - Adds support for translation of keys "value" and "default_value"

--- tools/tool_acf_translate/index.php
+++ tools/tool_acf_translate/index.php
@@ -117,7 +117,7 @@
 
 						// STRINGS {
 
-							$keys = array( 'title', 'page_title', 'menu_title', 'label', 'button_label', 'description', 'instructions', 'message' );
+							$keys = array( 'title', 'page_title', 'menu_title', 'label', 'button_label', 'description', 'instructions', 'message', 'value', 'default_value' );
 
 							if (
 								is_string( $item ) AND
@@ -146,7 +146,7 @@
 
 									// }
 
-									$keys = array( 'title', 'page_title', 'menu_title', 'label', 'button_label', 'description', 'instructions' );
+									$keys = array( 'title', 'page_title', 'menu_title', 'label', 'button_label', 'description', 'instructions', 'message', 'value', 'default_value' );
 
 									if (
 										is_string( $item ) AND
