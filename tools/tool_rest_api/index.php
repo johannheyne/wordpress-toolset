624ecb934868960c601f0e36b6ec85cc1e186104
johannheyne <mail@johannheyne.de>
2019-03-18 06:06:50 +0100

    Changes tool_rest_api
    changes depricated filter rest_enabled to rest_authentication_errors
    
    Signed-off-by: johannheyne <mail@johannheyne.de>

--- tools/tool_rest_api/index.php
+++ tools/tool_rest_api/index.php
@@ -8,8 +8,14 @@
 				! is_user_logged_in() AND // enables API for logged in users
 				! is_admin()
 			) {
-				add_filter( 'rest_enabled', '_return_false' );
-				add_filter( 'rest_jsonp_enabled', '_return_false' );
+				//add_filter( 'rest_enabled', '_return_false' );
+				//add_filter( 'rest_jsonp_enabled', '_return_false' );
+
+				add_filter( 'rest_authentication_errors', function( $access ) {
+
+					return new WP_Error( 'rest_disabled', __( 'The REST API on this site has been disabled.' ), array( 'status' => rest_authorization_required_code() ) );
+				} );
+
 			}
 
 		// }
