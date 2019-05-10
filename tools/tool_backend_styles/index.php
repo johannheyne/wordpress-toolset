fae025c970e4aeebae9ebf0f067692b6ab6e5679
johannheyne <mail@johannheyne.de>
2019-05-10 14:43:12 +0200

    Updates tool_backend_styles
    - Adds versioning

--- tools/tool_backend_styles/index.php
+++ tools/tool_backend_styles/index.php
@@ -4,7 +4,7 @@
 
 		add_action( 'admin_head', function() {
 
-			echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo( 'template_directory' ) . '/css/backend-styles.css" />';
+			echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo( 'template_directory' ) . '/css/backend-styles.css?v=' . config_get_theme_version() . '" />';
 
 		} );
 
