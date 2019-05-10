204e7fa799a2b2f233713466ce4a4a763dbb7498
johannheyne <mail@johannheyne.de>
2019-05-10 14:42:46 +0200

    Updates tool_backend_javascript

--- tools/tool_backend_javascript/index.php
+++ tools/tool_backend_javascript/index.php
@@ -2,11 +2,17 @@
 
 	// BACKEND JAVASCRIPT ( Version 1 ) {
 
-		function customBackendScript() {
+		/*function customBackendScript() {
 
 			echo '<script src="' . get_bloginfo('template_directory') . '/js/wp-backend.js" type="text/javascript"></script>';
 		}
 
 		add_action('admin_head', 'customBackendScript');
+		*/
+
+		add_action( 'admin_enqueue_scripts', function( $hook ) {
+
+			wp_enqueue_script( 'theme_backend_script', get_bloginfo('template_directory') . '/js/wp-backend.js?v=' . config_get_theme_version() );
+		});
 
 	// }
