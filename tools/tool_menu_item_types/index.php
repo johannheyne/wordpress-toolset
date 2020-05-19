1c930cd598a37aa142178f797f3a45d59ae09933
johannheyne <mail@johannheyne.de>
2020-05-19 05:13:56 +0200

    Changes tool tool_menu_item_types
    - detects custom link URL field values "mailto:" etc.

--- tools/tool_menu_item_types/index.php
+++ tools/tool_menu_item_types/index.php
@@ -2,38 +2,17 @@
 
 	if ( ! empty( $GLOBALS['toolset']['inits']['tool_menu_item_types'] ) ) {
 
-		if ( is_admin() ) {
-
-			add_action( 'admin_enqueue_scripts', function() {
-
-				wp_enqueue_script( 'tool_menu_item_types', $GLOBALS['toolset']['plugins_url'] . '/wordpress-toolset/tools/tool_menu_item_types/admin-script.js', array( 'jquery' ), config_get_theme_version() );
-				wp_enqueue_style( 'tool_menu_item_types', $GLOBALS['toolset']['plugins_url'] . '/wordpress-toolset/tools/tool_menu_item_types/admin-styles.css', array(), config_get_theme_version() );
-			});
-		}
-
-		add_filter( 'nav_menu_css_class', function( $classes, $item, $args, $depth ) {
-
-			$link_type = get_field( 'link_type', $item->ID );
-
-			if ( $link_type !== 'url' ) {
-
-				$classes[] = 'link-type-' . $link_type;
-			}
-
-			return $classes;
-		}, 11, 4 );
-
 		add_filter( 'nav_menu_link_attributes', function( $atts, $item, $args, $depth ) {
 
 			$link_type = get_field( 'link_type', $item->ID );
 
-			if ( $link_type === 'email' ) {
+			if ( trim( $atts['href'] ) === 'mailto:' ) {
 
 				$email = get_lang_field( 'opt_contactdata/email', 'option' );
 				$atts['href'] = 'mailto:' . antispambot( $email );
 			}
 
-			if ( $link_type === 'phone' ) {
+			if ( trim( $atts['href'] ) === 'tel:' ) {
 
 				$phone = get_lang_field( 'opt_contactdata/fon', 'option' );
 				$atts['href'] = get_tel_url( array(
@@ -41,7 +20,7 @@
 				));
 			}
 
-			if ( $link_type === 'fax' ) {
+			if ( trim( $atts['href'] ) === 'fax:' ) {
 
 				$fax = get_lang_field( 'opt_contactdata/fax', 'option' );
 				$atts['href'] = get_tel_url( array(
@@ -55,10 +34,7 @@
 
 		add_filter( 'nav_menu_item_title', function( $title, $item, $args, $depth ) {
 
-			$menu_name = get_lang_field( 'menu_item/menu_name', $item->ID );
-			$link_type = get_field( 'link_type', $item->ID );
-
-			if ( $link_type === 'email' ) {
+			if ( trim( $item->url ) === 'mailto:' ) {
 
 				if ( empty( $menu_name ) ) {
 
@@ -67,7 +43,7 @@
 				}
 			}
 
-			if ( $link_type === 'phone' ) {
+			if ( trim( $item->url ) === 'tel:' ) {
 
 				if ( empty( $menu_name ) ) {
 
@@ -76,7 +52,7 @@
 				}
 			}
 
-			if ( $link_type === 'fax' ) {
+			if ( trim( $item->url ) === 'fax:' ) {
 
 				if ( empty( $menu_name ) ) {
 
